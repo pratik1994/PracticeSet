d12c44436d0539cc08c839043fa2d7f433ed8224
pratik-mittal <pratik.mittal@driscolls.com>
2020-04-30 22:41:55 +0530

    are two arrays similar

---
+++ sameArray.js
@@ -0,0 +1,28 @@
+function same (arr1, arr2){
+    if(arr1.length !== arr2.length){
+        return false;
+    }
+    let freq1={}
+    let freq2={}
+
+    for (let val of arr1){
+        freq1[val] = (freq1[val] || 0) +1
+    }
+    for (let val of arr2){
+        freq2[val] = (freq2[val] || 0) +1
+    }
+    for (let key in freq1){
+
+        if(!(key**2 in freq2)){
+            console.log(key);
+            console.log(freq2)
+            return false
+        }
+        if(freq2[key**2] !== freq1[key]){
+            return false
+        }
+    }
+    return true;
+}
+
+//this is to check if two arrays are same or not
