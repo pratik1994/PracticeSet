07920ac5d04e20b3c8278dc87947b3711ee1e707
pratik-mittal <pratik.mittal@driscolls.com>
2020-04-30 22:41:20 +0530

    anagram

---
+++ anagram.js
@@ -0,0 +1,21 @@
+function anagram(str1, str2){
+    if (str1.length !== str2.length){
+        return false;
+    }
+    let obj={};
+    
+    for(char in str1){
+        obj[char] = (obj[char]||0)+1
+    }
+    console.log(obj)
+    for (char in str2){
+        if(!obj[char]){
+            return false
+        }else{
+            obj[char]= obj[char] - 1; 
+        }
+    }
+    return true
+}
+
+//this is to check whether two string are anagram of each other or not
